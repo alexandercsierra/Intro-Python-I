7af89186e019c69cd8228181a30659a60b48ed7f
Alexander Sierra <AlexanderCSierra@alexanderrasmbp.lan>
2020-05-04 14:52:26 -0400

    completed 13

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,6 +10,10 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
 # YOUR CODE HERE
+f = open("foo.txt", "r")
+print(f.read())
+f.close()
+
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -17,3 +21,11 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+myFile = open('test.txt', 'w')
+myFile.write('This will always be the first line. ')
+myFile.close()
+
+myFile = open('test.txt', 'a')
+myFile.write('And this line will be appended afterwards')
+myFile.close()
+
