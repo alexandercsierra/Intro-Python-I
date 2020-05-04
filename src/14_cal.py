4e3e87866c19c6a47e9a840a53699112043ec436
Alexander Sierra <AlexanderCSierra@alexanderrasmbp.lan>
2020-05-04 16:54:17 -0400

    14 mostly done

--- src/14_cal.py
+++ src/14_cal.py
@@ -30,3 +30,27 @@ it should use today’s date to get the month and year.
 import sys
 import calendar
 from datetime import datetime
+
+# print(calendar.month(20, 5)) 
+
+def calFunc():
+  ans = input('Which calendar would you like to see?').split()
+  y = "20"
+  m = "05"    
+  if len(ans) == 1:
+    m = ans[0]
+  elif len(ans) == 2:
+    m = ans[0]
+    y = ans[1]
+  if m.isdigit() == False or y.isdigit() == False:
+    return print('please enter numbers')
+  
+  year = int(y)
+  month = int(m)
+  if month > 12 or month < 1:
+    return print('please enter a valid month')
+  print(calendar.month(year, month))
+  print(f'month is {m}')
+  print(f'year is {y}')
+
+calFunc()
