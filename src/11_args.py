0be770bf2d3c07d836f8decd284e06f0773fd9d1
Alexander Sierra <AlexanderCSierra@alexanderrasmbp.lan>
2020-05-04 14:40:27 -0400

    completed most of 11

--- src/11_args.py
+++ src/11_args.py
@@ -5,6 +5,8 @@
 # the sum. This is what you'd consider to be a regular, normal function.
 
 # YOUR CODE HERE
+def f1(num1, num2):
+    return num1 + num2
 
 print(f1(1, 2))
 
@@ -13,6 +15,16 @@ print(f1(1, 2))
 # Note: Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
+
+def f2(*args):
+    total = 0
+    for arg in args:
+        if type(arg) == list:
+            for item in arg:
+                total += item
+        else:
+            total += arg
+    return total
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -31,6 +43,12 @@ print(f2(a))    # Should print 22
 
 # YOUR CODE HERE
 
+def f3 (num, num2=None):
+    if num2 == None:
+        return num + 1
+    else:
+        return num + num2
+
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
 
@@ -44,6 +62,9 @@ print(f3(8))     # Should print 9
 # Note: Google "python keyword arguments".
 
 # YOUR CODE HERE
+def f4(**kwargs):
+        for i, j in kwargs.items():
+            print ("key: {}, value: {}".format(i,j))
 
 # Should print
 # key: a, value: 12
@@ -62,4 +83,6 @@ d = {
 }
 
 # How do you have to modify the f4 call below to make this work?
-f4(d)
+
+# ?????? No idea what it wants me to do
+# f4(d)
