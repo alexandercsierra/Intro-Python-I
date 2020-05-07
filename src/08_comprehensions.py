53c653b2262fde71f36447de5c49e97264c0e1f7
Alexander Sierra <AlexanderCSierra@alexanderrasmbp.lan>
2020-05-07 16:47:08 -0400

    fixed last comprehension

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -32,9 +32,10 @@ print(y)
 # Use a list comprehension to create a list containing only the _even_ elements
 # the user entered into list x.
 
-x = input("Enter comma-separated numbers: ")
+x = input("Enter comma-separated numbers: ").split(",")
+print(x)
 
 # What do you need between the square brackets to make it work?
-y = [num for num in x if num%2==0]
-
+y = [num for num in x if int(num)%2==0]
 print(y)
+
